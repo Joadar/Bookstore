7cc5525162892818a3b07aed506927f29d4804d3
Jonathan <jonathan.pannetier@gmail.com>
2016-02-29 15:57:33 +0100

    Add some Integrations Test

--- Android/app/src/androidTest/java/com/exemple/bookstore/MainActivityTest.java
+++ Android/app/src/androidTest/java/com/exemple/bookstore/MainActivityTest.java
@@ -3,6 +3,7 @@ package com.exemple.bookstore;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.widget.EditText;
 
 import com.exemple.bookstore.Activities.MainActivity;
 
@@ -11,11 +12,16 @@ import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressKey;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.action.ViewActions.swipeUp;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
 
 @LargeTest
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
@@ -47,5 +53,21 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     @Test
     public void testCommentsIsDisplayed(){
         //onView(withText(R.string.comments)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSearchFalseBooks() throws InterruptedException {
+        onView(withId(R.id.action_search)).perform(click());
+        onView(isAssignableFrom(EditText.class)).perform(typeText("test"), pressKey(66));
+        Thread.sleep(1500);
+        onView(withText(R.string.emptyBooks)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSearchRealBooks() throws InterruptedException {
+        onView(withId(R.id.action_search)).perform(click());
+        onView(isAssignableFrom(EditText.class)).perform(typeText("halo"), pressKey(66));
+        Thread.sleep(1500);
+        onView(withText(R.string.emptyBooks)).check(matches(not(isDisplayed())));
     }
 }
